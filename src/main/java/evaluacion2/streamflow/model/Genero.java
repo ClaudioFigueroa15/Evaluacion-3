a7f2812269d5906bfde5463dd15a7967ef0aaaa3
ClaudioFigueroa15 <cl.figueroac@duocuc.cl>
2026-06-10 21:12:07 -0400

    Proyecto Base
    Se sube al repositorio el proyecto base

---
+++ src/main/java/evaluacion2/streamflow/model/Genero.java
@@ -0,0 +1,29 @@
+package evaluacion2.streamflow.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "generos")
+public class Genero {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String nombre;
+
+    @Column(length = 255)
+    private String descripcion;
+
+    @OneToMany(mappedBy = "genero", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Pelicula> peliculas;
+}
